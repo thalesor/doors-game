ed245ba18cfc05bcbdc9a8b0f3bf507ad9cfcc07
Thales Ruano <thalesoruano@gmail.com>
2022-07-07 01:10:50 -0300

    feat: add events on door component clicks

--- components/Door.tsx
+++ components/Door.tsx
@@ -2,18 +2,23 @@ import styles from "../styles/Door.module.css";
 import DoorModel from "../models/door";
 
 interface IDoorProps {
-    door: DoorModel;
+    value: DoorModel;
+    onChange: (newDoor: DoorModel) => void;
 }
 
 export default function Door(props: IDoorProps)
 {
-    const { door } = props;
+    const { value, onChange } = props;
 
     return (
-            <div className={styles.outerRegion}>
-                <div className={`${styles.frame} ${door.selected && styles.selected}`}>
-                    <div className={styles.number}>{door.number}</div>
-                    <div className={styles.knob} />
+            <div className={styles.outerRegion} onClick={() => onChange(value.toggleSelection())}>
+                <div className={`${styles.frame} ${value.selected && styles.selected}`}>
+                    {!value.open &&
+                    <>
+                        <div className={styles.number}>{value.number}</div>
+                        <div className={styles.knob} />
+                    </>
+                    }   
                 </div>
                 <div className={styles.floor}/>
             </div>
